6f2d9aac3cef6d0dccccb01d6c4baaf9fcbf1f8e
MAD20011 <160303021+MAD20011@users.noreply.github.com>
2025-12-10 22:11:42 -0600

    Update create_tables.sql

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,23 @@
 -- SQL para crear tablas en SAP HANA Cloud
+
+ INVENTARIO Y VENTAS
+
+CREATE TABLE ventas (
+    id_venta INTEGER,
+    fecha DATE,
+    pais NVARCHAR(10),
+    almacen NVARCHAR(30),
+    id_producto INTEGER,
+    nombre_producto NVARCHAR(100),
+    cantidad INTEGER,
+    precio DECIMAL(10,2)
+);
+
+CREATE TABLE inventario (
+    id_inventario INTEGER,
+    fecha DATE,
+    pais NVARCHAR(10),
+    almacen NVARCHAR(30),
+    id_producto INTEGER,
+    stock INTEGER
+);
