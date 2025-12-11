b6c9c87b71ea6422634b258d62f8636d4a658c82
MAD20011 <160303021+MAD20011@users.noreply.github.com>
2025-12-10 22:12:45 -0600

    Add data loading from CSV files for ventas and inventario
    Added SQL commands to import data from multiple CSV files into ventas and inventario tables.

--- sql/ingest_load.sql
+++ sql/ingest_load.sql
@@ -1,1 +1,34 @@
+CARGA DE DATOS DESDE ARCHIVOS CSV
+-- Cargar ventas
+IMPORT FROM CSV FILE './data/ventas_mex.csv'
+INTO ventas
+WITH RECORD DELIMITED BY '\n'
+FIELD DELIMITED BY ',';
+
+IMPORT FROM CSV FILE './data/ventas_esp.csv'
+INTO ventas
+WITH RECORD DELIMITED BY '\n'
+FIELD DELIMITED BY ',';
+
+IMPORT FROM CSV FILE './data/ventas_usa.csv'
+INTO ventas
+WITH RECORD DELIMITED BY '\n'
+FIELD DELIMITED BY ',';
+
+
+-- Cargar inventarios
+IMPORT FROM CSV FILE './data/inventario_mex.csv'
+INTO inventario
+WITH RECORD DELIMITED BY '\n'
+FIELD DELIMITED BY ',';
+
+IMPORT FROM CSV FILE './data/inventario_esp.csv'
+INTO inventario
+WITH RECORD DELIMITED BY '\n'
+FIELD DELIMITED BY ',';
+
+IMPORT FROM CSV FILE './data/inventario_usa.csv'
+INTO inventario
+WITH RECORD DELIMITED BY '\n'
+FIELD DELIMITED BY ',';
 
